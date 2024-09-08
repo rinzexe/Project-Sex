8d292b66d3eeb5a9b5c21159d910fe459863b147
rinz <roberts.denisovs@gmail.com>
2024-09-08 21:38:28 +0300

    uhhhh

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -7,4 +7,16 @@ public class Building : MonoBehaviour
     public float detectionRange;
     public float coldResistance;
     public float health;
+
+    public List<GameObject> targets;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        targets.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        targets.Remove(collision.gameObject);
+    }
 }
--- Assets/Scripts/BuildingController.cs
+++ Assets/Scripts/BuildingController.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class BuildingController : Building
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    GameObject target;
+
+    private void Start()
     {
-        
+        InvokeRepeating("Fire", 0.0f, 1.0f);
+    }
+
+    void Fire()
+    {
+        GetClosestTarget();
+    }
+
+    void GetClosestTarget()
+    {
+        if (targets.Count > 0)
+        {
+            GameObject closest = targets[0];
+            foreach (var itarget in targets)
+            {
+                if (Vector3.Distance(itarget.transform.position, transform.position) < Vector3.Distance(transform.position, closest.transform.position))
+                {
+                    closest = itarget;
+                }
+            }
+            target = closest;
+        }
     }
 }
